14dd142b30452ff321e1343d40d158bc98191a4c
joaquinnn26 <joaquinfefe@gmail.com>
2025-08-15 22:31:43 -0300

    corrigiendo errores

--- js/script.js
+++ js/script.js
@@ -58,7 +58,7 @@ document.getElementById('formMudanza').addEventListener('submit', async function
         detalle: document.getElementById('detalle').value
     };
 
-    const res = await fetch('http://https://backmudaflet2025.onrender.com/api/mudanza', {
+    const res = await fetch('https://backmudaflet2025.onrender.com/api/mudanza', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(data)
